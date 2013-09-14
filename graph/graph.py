e34d811369b9ec1dac2a4f5fe44d6c8aa970348e
camila-maia <camilinhamaia120050031@gmail.com>
2013-09-14 11:03:52 -0300

    Finished graph functions

--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from node import Node
+import random
 
 class Graph():
 	
@@ -10,7 +11,8 @@ class Graph():
 
     def success_method(self):
     	self.test_node()    
-    	self.add_node(5)
+    	self.print_graph()
+        self.add_node(5)
         self.print_graph()
         # self.remove_node(10)
         # self.print_graph()
@@ -18,6 +20,11 @@ class Graph():
         self.print_graph()
         self.disconnect_nodes(5,10)
         self.print_graph()
+        # self.get_nodes()
+        # print self.get_random_node()
+        # print self.get_adj_nodes(10)
+        # print self.size(10)
+
 
     def add_node(self, node_value):
         if node_value not in self.nodes:
@@ -55,11 +62,33 @@ class Graph():
         else:
             print "Error disconnecting: The two nodes must be on graph"
 
+    def order(self):
+        return len(self.nodes)
+
+    def get_nodes(self):
+        return set(self.nodes.keys())        
+
+    def get_random_node(self):
+        return random.choice(self.nodes.keys())
+
+    def get_adj_nodes(self, node_value):
+        if node_value in self.nodes:
+            node = self.nodes[node_value]
+            return set(node.adjacent_nodes.keys())
+        print "Error getting adjacent nodes: There is no node " + str(node_value) + " on Graph"
+
+    def size(self, node_value):
+        if node_value in self.nodes:
+            node = self.nodes[node_value]
+            return node.qnt_adj_nodes()
+        print "Error getting size: There is no node " + str(node_value) + " on Graph"            
+
     def print_graph(self):
-        print "Graph: \n{"
+        print "Graph: \n  {"
         for node_value in self.nodes:
-    		print '  ', node_value, self.nodes[node_value].adjacent_nodes
-        print "}"
+    		print '    ', node_value, self.nodes[node_value].adjacent_nodes
+        print "  }"
+        print "  order: ", str(self.order()), "\n" 
 
     def test_node(self):
         node = Node(10)
