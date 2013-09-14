f86e9c03b076a7f8f3d67c20ad954bbdff78cc93
camila-maia <camilinhamaia120050031@gmail.com>
2013-09-14 10:07:13 -0300

    Added remove_node function

--- graph/graph.py
+++ graph/graph.py
@@ -5,31 +5,48 @@ from node import Node
 class Graph():
 	
     def __init__(self):
-    	self.nodes = set()
+    	self.nodes = {}
     	pass
 
     def success_method(self):
-    	# self.test_node()    
+    	self.test_node()    
     	self.add_node(5)
-    	self.print_nodes()
+        self.print_graph()
+        self.remove_node(10)
+        self.print_graph()
 
     def add_node(self, node_value):
-    	node = Node(node_value)
-    	self.nodes.add(node)
+        if node_value not in self.nodes:
+        	node = Node(node_value)
+        	self.nodes.update({node.value:node})
+        else:
+            print "Error adding: The node " + str(node_value) + " is already on graph."
 
     def remove_node(self, node_value):
-    	pass
+    	if node_value in self.nodes:
+            del self.nodes[node_value]
+            for node in self.nodes.values():
+                if node_value in node.adjacent_nodes:
+                    node.remove_adj_node(node_value)
+        else:
+            print "Error removing: There is no node " + str(node_value) + " on Graph"
 
-    def print_nodes(self):
-    	for node in self.nodes:
-    		print node.value
+    def conect_nodes(self, node_value1, node_value2) :
+        pass
+
+    def print_graph(self):
+        print "Graph: \n{"
+        for node_value in self.nodes:
+    		print '  ', node_value, self.nodes[node_value].adjacent_nodes
+        print "}"
 
     def test_node(self):
-        node = Node(5)
-    	print node.adjacent_nodes, "length: ", node.qnt_adj_nodes()
-    	node.add_adj_node(2,3)
-    	print node.adjacent_nodes, "length: ", node.qnt_adj_nodes()
-    	node.add_dict_adj_nodes({6:9,7:1})
-    	print node.adjacent_nodes, "length: ", node.qnt_adj_nodes()
-    	node.remove_adj_node(6)
-    	print node.adjacent_nodes, "length: ", node.qnt_adj_nodes()
+        node = Node(10)
+        self.nodes.update({node.value:node})
+        self.print_graph()
+        node.add_adj_node(2,3)
+        self.print_graph()
+        node.add_dict_adj_nodes({6:9,5:1})
+        self.print_graph()
+        node.remove_adj_node(6)
+        self.print_graph()
